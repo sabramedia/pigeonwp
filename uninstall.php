b7259686388d48c7d9a0f0037ca45800c78c006f
Matt Geri <mattgeri@gmail.com>
2014-07-13 16:58:44 +0200

    Updating the uninstall docs

--- uninstall.php
+++ uninstall.php
@@ -2,11 +2,11 @@
 /**
  * Fired when the plugin is uninstalled.
  *
- * @package   Plugin_Name
+ * @package   WP_Pidgeon
  * @author    Your Name <email@example.com>
  * @license   GPL-2.0+
- * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
+ * @link      http://pigeonpaywall.com/
+ * @copyright 2014 Sabramedia
  */
 
 // If uninstall not called from WordPress, then exit
