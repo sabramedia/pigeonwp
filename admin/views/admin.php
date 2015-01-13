72da82049bb6bac482cc00ef72d7699f8a202407
Jonathan Wold <howdy@jonathanwold.com>
2015-01-13 10:56:28 -0800

    Added plugin options to the setting screen.
    Options are accessible via get_option(‘wp_pigeon_settings’), which will
    return an array.

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -1,3 +1,18 @@
-<h2>Pigeon for WordPress</h2>
 
-<p>Admin options are coming soon. In the meantime, contact the Pigeon support team for any questions.</p>
+<div class="wrap">
+
+	<h2>Pigeon for WordPress</h2>
+	
+	<p>For questions regarding any of these settings please contact Pigeon support.</p>
+
+	<form action='options.php' method='post'>
+			
+		<?php
+		settings_fields( 'plugin_options' );
+		do_settings_sections( 'plugin_options' );
+		submit_button();
+		?>
+		
+	</form>
+
+</div>
