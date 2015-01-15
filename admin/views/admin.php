f80150944d27806ccf5c19f5d9cba1f82391890a
Nick Johnson <nick@sabramedia.com>
2015-01-15 13:17:06 -0500

    Added link to Pigeon Control Panel

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -1,9 +1,12 @@
-
+<?php
+$admin_options = get_option( 'wp_pigeon_settings' );
+$admin_options["pigeon_subdomain"] = str_replace(array("https://","http://"),"",$admin_options["pigeon_subdomain"]);
+?>
 <div class="wrap">
 
 	<h2>Pigeon for WordPress</h2>
 	
-	<p>For questions regarding any of these settings please contact Pigeon support.</p>
+	<p>For questions regarding any of these settings please contact Pigeon support. <?php echo $admin_options["pigeon_subdomain"] ? "<a href=\"http://".$admin_options["pigeon_subdomain"]."/admin\" target=\"_blank\">Click here to access the Pigeon control panel</a>." : ""; ?> </p>
 
 	<form action='options.php' method='post'>
 			
