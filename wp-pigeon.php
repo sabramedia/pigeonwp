a53d33c70e1c857a136727f46a4d68edb3dfab2f
Nick Johnson <nick@sabramedia.com>
2016-11-11 15:10:30 -0500

    Corrected reference

--- wp-pigeon.php
+++ wp-pigeon.php
@@ -21,8 +21,8 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
  *
- * GitHub Theme URI: https://github.com/sabramedia/pigeonwp
- * GitHub Branch:    master
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
+ * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
