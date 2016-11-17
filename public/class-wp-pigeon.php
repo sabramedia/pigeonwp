613f2c531c56d2cc273d7e06cada656c1d89609c
Nick Johnson <nick@sabramedia.com>
2016-11-17 13:27:08 -0500

    SSO - Revert testing
    Sorted out SSO sync issues.

--- public/class-wp-pigeon.php
+++ public/class-wp-pigeon.php
@@ -636,13 +636,13 @@ class WP_Pigeon {
 //			print_r($this->pigeon_values);
 			// If SSO and the user is not logged in on Pigeon, then make sure WP is logged out
 			if( $admin_options["pigeon_wp_sso"] == 1 ){
-
 				if( ! $this->pigeon_values["profile"]  ){
 					if( is_user_logged_in() ){
 						// Only logout accounts that are linked by pigeon_customer_id
 						$pigeon_customer_id = get_user_meta(get_current_user_id(),'pigeon_customer_id', TRUE);
 						if( $pigeon_customer_id ){
-							echo "<!-- PIGEON HAS PROFILE".$pigeon_customer_id." -->";
+							wp_logout();
+							header("Refresh:0");
 						}
 					}
 				}else{
@@ -675,8 +675,9 @@ class WP_Pigeon {
 					}else{
 						// Only logout accounts that are linked by pigeon_customer_id
 						$pigeon_customer_id = get_user_meta(get_current_user_id(),'pigeon_customer_id', TRUE);
-						if( $pigeon_customer_id ){
-							echo "<!-- PIGEON NO PROFILE".$pigeon_customer_id." -->";
+						if( $this->pigeon_values["profile"]["customer_id"] != $pigeon_customer_id ){
+							wp_logout();
+							header("Refresh:0");
 						}
 					}
 				}
