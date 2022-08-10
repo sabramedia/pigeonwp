5417b7a21f58d1deae30373d66ebd6a1f96e8e11
Nick Johnson <nick@sabramedia.com>
2022-08-10 12:45:23 -0400

    SDK Curl response parsing update

--- sdk/Pigeon/CurlResponse.php
+++ sdk/Pigeon/CurlResponse.php
@@ -8,14 +8,14 @@ class CurlResponse
 	public function __construct($response)
 	{
 		// Extract headers from response
-		$pattern = '#HTTP/[\d|\d\.\d].*?$.*?\r\n\r\n#ims';
+		$pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
 		preg_match_all($pattern, $response, $matches);
 		$headers = preg_split("/\r\n/", str_replace("\r\n\r\n",'', array_pop($matches[0])));
 
 
 		// Extract the version and status from the first header
 		$version_and_status = array_shift($headers);
-		preg_match('#HTTP/([\d|\d\.\d])\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
+		preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
 		$this->headers['Http-Version'] = $matches[1];
 		$this->headers['Status-Code'] = $matches[2];
 		$this->headers['Status'] = $matches[2].' '.$matches[3];
